5fc1ecfcf531f007543d72944b77c13e25fa8d0e
Roger Viñas Alcon <roger.vinas@fastzink.com>
2023-12-01 15:39:14 +0100

    More things (#17)
    * More things
    
    * Update java and kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,19 +5,20 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
   id("org.springframework.boot") version "2.7.8"
   id("io.spring.dependency-management") version "1.0.15.RELEASE"
-  kotlin("jvm") version "1.6.21"
-  kotlin("plugin.spring") version "1.6.21"
+  kotlin("jvm") version "1.9.21"
+  kotlin("plugin.spring") version "1.9.21"
 }
 
 group = "com.rogervinas"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
   mavenCentral()
 }
 
 val springCloudVersion = "2021.0.8"
+val testContainersVersion = "1.19.3"
 
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -31,7 +32,9 @@ dependencies {
 
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.apache.kafka:kafka-streams-test-utils")
-  testImplementation("org.testcontainers:junit-jupiter:1.19.3")
+
+  testImplementation("org.testcontainers:junit-jupiter:$testContainersVersion")
+  testImplementation("org.testcontainers:testcontainers:$testContainersVersion")
 }
 
 dependencyManagement {
@@ -43,7 +46,7 @@ dependencyManagement {
 tasks.withType<KotlinCompile> {
   kotlinOptions {
     freeCompilerArgs = listOf("-Xjsr305=strict")
-    jvmTarget = "11"
+    jvmTarget = "17"
   }
 }
 
